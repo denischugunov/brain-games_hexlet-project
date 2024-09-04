62cc1211675da5c5aeafbf755b78dc40e05b719a
denis c. <denis.chugunov98@gmail.com>
2024-09-04 11:50:21 +0300

    add brain-even game

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import test from '../src/games/even_odd_game.js';
+
+test();
---
+++ src/games/even_odd_game.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const getRandomNum = () => Math.round(Math.random() * 100);
+  const isEven = (num) => num % 2 === 0;
+
+  let correctAnswer = 0;
+
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const checkEvenOdd = () => {
+    const randomNum = getRandomNum();
+    console.log(`Question: ${randomNum}`);
+    const expectedAnswer = isEven(randomNum) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === expectedAnswer) {
+      console.log('Correct!');
+      correctAnswer += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is the wrong answer ;(. Correct answer was '${expectedAnswer}'.`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    if (correctAnswer === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    } else {
+      checkEvenOdd();
+    }
+  };
+
+  checkEvenOdd();
+};
